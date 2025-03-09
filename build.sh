7c527d01b956573e346e9e847f01a4d997fa1379
Brad Feld <brad@feld.com>
2025-03-09 15:06:00 -0700

    Fix Render deployment: Replace psycopg2-binary with psycopg and remove apt-get commands

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-echo "Installing system dependencies..."
-apt-get update
-apt-get install -y libpq-dev python3-dev
-
 echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
